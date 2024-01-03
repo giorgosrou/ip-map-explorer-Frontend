26e14470707017bcf82210b2ba82631f53104df5
Georgios Rounis <georg.rounis@gmail.com>
2024-01-03 23:02:09 +0100

    minor fix

--- ip-map-explorer/src/Components/CustomMap.tsx
+++ ip-map-explorer/src/Components/CustomMap.tsx
@@ -9,43 +9,23 @@ const CustomMap: React.FC = () => {
     const [userRegion, setUserRegion] = useState('');
     const [userCity, setUserCity] = useState('');
 
-    //Currently not used - Call the api.ipify api from the backend to retrieve ip - ip-api is used instead
-    // useEffect(() => {
-    //     fetch('http://localhost:3000/getip')
-    //       .then((response) => response.text())
-    //       .then((text) => {
-    //         try {
-    //           const data = JSON.parse(text);
-    //           setUserIP(data.ip);
-    //         } catch (error) {
-    //           console.error('Error parsing JSON:', error);
-    //           setUserIP('Invalid IP data');
-    //         }
-    //       })
-    //       .catch((error) => {
-    //         console.error('Error fetching IP:', error);
-    //       });
-    //   }, []);
-
     //Call the ip-api api from the backend to retrieve ip, country, region and city
     useEffect(() => {
       fetch('http://localhost:3000/ipapi')
-        .then((response) => response.text())
-        .then((text) => {
-          try {
-            const data = JSON.parse(text);
-            setUserIP(data.query);
-            setUserCountry(data.country)
-            setUserRegion(data.regionName)
-            setUserCity(data.city)
-            setPositionIP({lat: data.lat, lng: data.lon})
-          } catch (error) {
-            console.error('Error parsing JSON:', error);
-            setUserIP('Invalid IP data');
-          }
+        .then((response) => response.json())
+        .then((data) => {
+          setUserIP(data.query);
+          setUserCountry(data.country);
+          setUserRegion(data.regionName);
+          setUserCity(data.city);
+          setPositionIP({ lat: data.lat, lng: data.lon });
         })
         .catch((error) => {
           console.error('Error fetching IP:', error);
+          setUserIP('Invalid IP data');
+          setUserCountry('Unknown');
+          setUserRegion('Unknown');
+          setUserCity('Unknown');
         });
     }, []);
 
