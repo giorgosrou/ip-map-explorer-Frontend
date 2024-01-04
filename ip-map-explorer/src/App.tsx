915f525c0ab62a076be1f5e4f5ada46a7da66a06
Georgios Rounis <georg.rounis@gmail.com>
2024-01-04 11:11:26 +0100

    refactor code

--- ip-map-explorer/src/App.tsx
+++ ip-map-explorer/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import GoogleMap from './Components/GoogleMap';
 import Logo from './Components/Logo';
-import ParticlesBg from 'particles-bg'
 import WebsiteVisitorsInfoSearch from './Components/WebsiteVisitorsInfoSearch';
 import CurrentUserInfo from './Components/CurrentUserInfo';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="flex flex-col bg-gradient-to-b from-indigo-900 to-gray-900">
       <div className="flex-1 overflow-y-auto">
-        <ParticlesBg type="lines" bg={true} />
       </div>
       <div className="flex-1">
         <Logo />
--- ip-map-explorer/src/Components/CurrentUserInfo.tsx
+++ ip-map-explorer/src/Components/CurrentUserInfo.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FaFlag, FaMapMarkerAlt } from "react-icons/fa"
+import { FaFlag, FaMapMarkerAlt } from "react-icons/fa";
 
 const CurrentUserInfo: React.FC = () => {
 
